bfee60ce31ad83ecbcb0b4270b75dee1610c6f29
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-14 15:36:08 -0300

    Hello World JSON version

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send('hello world');
+    return response.json({ message: 'Hello World'});
 });
 
 app.listen(3333);
