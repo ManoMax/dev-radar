6c9d13ea2dd4986f8fa0d23fcd74ada59fc84160
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-15 11:53:12 -0300

    GET List Devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,6 +4,12 @@ const Dev = require('../models/Dev');
 // index, show, store, update, destroy
 
 module.exports = {
+    async index(request, response) {
+        const devs = await Dev.find();
+    
+        return response.json(devs);
+    },
+
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
     
