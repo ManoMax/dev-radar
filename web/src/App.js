672330c3f68069c56eefa2bacd293569c0deafc4
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-15 19:20:34 -0300

    Usuários Github listados

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,6 +7,8 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+  
   const [github_username, setGithubUsername] = useState('');
   const [techs, setTechs] = useState('');
   const [latitude, setLatitude] = useState('');
@@ -28,11 +31,35 @@ function App() {
     )
   }, []);
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, [])
+  
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_user">Usuário do Github</label>
             <input
@@ -84,50 +111,19 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://scontent.fcpv4-1.fna.fbcdn.net/v/t1.0-9/23844636_1961214077539405_8281318683202728329_n.jpg?_nc_cat=109&_nc_ohc=8sLlQSAW9v0AX_vIr1v&_nc_ht=scontent.fcpv4-1.fna&oh=265a7b940cbb99cf705baec7682c4687&oe=5EAA0A43" alt="ManoMax"/>
-              <div className="user-info">
-                <strong>ManoMax</strong>
-                <span>JS, Java, Python</span>
-              </div>
-            </header>
-            <p>Programming since 2017 ....................................................................... Perfil Photo by: @EuclidesRamos .</p>
-            <a href="https://github.com/ManoMax">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://scontent.fcpv4-1.fna.fbcdn.net/v/t1.0-9/23844636_1961214077539405_8281318683202728329_n.jpg?_nc_cat=109&_nc_ohc=8sLlQSAW9v0AX_vIr1v&_nc_ht=scontent.fcpv4-1.fna&oh=265a7b940cbb99cf705baec7682c4687&oe=5EAA0A43" alt="ManoMax"/>
-              <div className="user-info">
-                <strong>ManoMax</strong>
-                <span>JS, Java, Python</span>
-              </div>
-            </header>
-            <p>Programming since 2017 ....................................................................... Perfil Photo by: @EuclidesRamos .</p>
-            <a href="https://github.com/ManoMax">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://scontent.fcpv4-1.fna.fbcdn.net/v/t1.0-9/23844636_1961214077539405_8281318683202728329_n.jpg?_nc_cat=109&_nc_ohc=8sLlQSAW9v0AX_vIr1v&_nc_ht=scontent.fcpv4-1.fna&oh=265a7b940cbb99cf705baec7682c4687&oe=5EAA0A43" alt="ManoMax"/>
-              <div className="user-info">
-                <strong>ManoMax</strong>
-                <span>JS, Java, Python</span>
-              </div>
-            </header>
-            <p>Programming since 2017 ....................................................................... Perfil Photo by: @EuclidesRamos .</p>
-            <a href="https://github.com/ManoMax">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://scontent.fcpv4-1.fna.fbcdn.net/v/t1.0-9/23844636_1961214077539405_8281318683202728329_n.jpg?_nc_cat=109&_nc_ohc=8sLlQSAW9v0AX_vIr1v&_nc_ht=scontent.fcpv4-1.fna&oh=265a7b940cbb99cf705baec7682c4687&oe=5EAA0A43" alt="ManoMax"/>
-              <div className="user-info">
-                <strong>ManoMax</strong>
-                <span>JS, Java, Python</span>
-              </div>
-            </header>
-            <p>Programming since 2017 ....................................................................... Perfil Photo by: @EuclidesRamos .</p>
-            <a href="https://github.com/ManoMax">Acessar Perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil no Github</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
