21543ee1f64aac071c86e310619a29e2595983bd
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-15 15:58:07 -0300

    Conceito Estado

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-// Estado: Informações que um componente PAI passa para o componente FILHO
-// Propriedade
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 1);
+  };
+
   return (
     <>
-      <Header title="Titulo 1" />
-      <Header title="Titulo 2" />
-      <Header title="Titulo 3" />
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
     </>
   );
 }
