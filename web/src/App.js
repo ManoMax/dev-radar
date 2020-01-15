eadda498f1357e1ebf6575d1faa47838337143fd
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-15 15:49:53 -0300

    Conceito Propriedade

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-// Estado
+// Estado: Informações que um componente PAI passa para o componente FILHO
 // Propriedade
 
 import Header from './Header';
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header title="Titulo 1" />
+      <Header title="Titulo 2" />
+      <Header title="Titulo 3" />
+    </>
   );
 }
 
