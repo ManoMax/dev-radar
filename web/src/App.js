aaf7205fa934a77f7cf9936be4b7e1962ef6ae25
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-15 19:35:37 -0300

    Components for Devs

--- web/src/App.js
+++ web/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+
 function App() {
   const [devs, setDevs] = useState([]);
   
@@ -114,17 +116,7 @@ function App() {
       <main>
         <ul>
           {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name} />
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil no Github</a>
-            </li>
+            <DevItem key={dev._id} dev={dev}/>
           ))}
         </ul>
       </main>
