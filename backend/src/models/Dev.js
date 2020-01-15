1ac772a6656cb856a51647a19a660b2f861c2cdd
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-15 11:47:19 -0300

    Implementação de GeoLocalização no Dev

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const PointSchema = require('./utils/PointSchema');
 
-const devSchema = new mongoose.Schema({
+const DevSchema = new mongoose.Schema({
     name: String,
     github_username: String,
     bio: String,
     avatar_url: String,
-    techs: [String]
+    techs: [String],
+    location: {
+        type: PointSchema,
+        index: '2dsphere'
+    }
 });
 
 module.exports = mongoose.model('Dev', DevSchema);
