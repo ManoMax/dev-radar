9b0dee89471cd3012607674112e3cea9066d3d73
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-18 16:56:12 -0300

    Config App e Adc GeoMap, via React Native

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+
+function Main() {
+    return <MapView style={styles.map}/>
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+})
+
+export default Main;
