98b600061cc2af308dc022c186746b6fa410abf1
ManoMax <gabrielmaxcontato@gmail.com>
2020-01-19 17:21:47 -0300

    Removendo aviso de erro

--- mobile/App.js
+++ mobile/App.js
@@ -3,7 +3,9 @@ import { StatusBar, YellowBox } from 'react-native';
 
 import Routes from './src/routes';
 
-
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return (
